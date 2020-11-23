2221bb31e0ba9f04ea39db035be0f1ae7c1c6355
sarahloyd-usf <sarahloyd@usf.edu>
2020-11-22 19:44:44 -0500

    JSON parsing into object

--- API_Project/Controllers/HomeController.cs
+++ API_Project/Controllers/HomeController.cs
@@ -6,11 +6,18 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using API_Project.Models;
+using System.Net.Http;
+using Newtonsoft.Json;
 
 namespace API_Project.Controllers
 {
     public class HomeController : Controller
     {
+        HttpClient httpClient;
+
+        static string BASE_URL = "https://data.cdc.gov/resource/6vqh-esgs.json";
+        //        static string API_KEY = "mdBybOievMdeX3eYSC0MhFu3U7xRV18xHAPG04qb"; //Add your API key here inside ""
+
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
@@ -25,6 +32,41 @@ namespace API_Project.Controllers
 
         public IActionResult Data()
         {
+            httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Accept.Clear();
+            //            httpClient.DefaultRequestHeaders.Add("X-Api-Key", API_KEY);
+            httpClient.DefaultRequestHeaders.Accept.Add(
+                new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+            //            string NATIONAL_PARK_API_PATH = BASE_URL + "/parks?limit=20";
+            string covidDataStr = "";
+
+            MyClass myclass = null;
+
+            httpClient.BaseAddress = new Uri(BASE_URL);
+
+            try
+            {
+                HttpResponseMessage response = httpClient.GetAsync(BASE_URL).GetAwaiter().GetResult();
+
+                if (response.IsSuccessStatusCode)
+                {
+                    covidDataStr = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+                }
+
+                if (!covidDataStr.Equals(""))
+                {
+                    //fix this crappy json
+                    covidDataStr = "{\"data\":" + covidDataStr + "}";
+                    // JsonConvert is part of the NewtonSoft.Json Nuget package
+                    myclass = JsonConvert.DeserializeObject<MyClass>(covidDataStr);
+                }
+            }
+            catch (Exception e)
+            {
+                // This is a useful place to insert a breakpoint and observe the error message
+                Console.WriteLine(e.Message);
+            }
             return View();
         }
         public IActionResult Symptoms()
@@ -48,3 +90,8 @@ namespace API_Project.Controllers
         }
     }
 }
+
+
+
+
+
---
+++ API_Project/Models/App_Models.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_Project.Models
+{
+//    public class App_Models
+  //  {
+        //public class Users
+        //{
+        //    public int userid { get; set; }
+        //    public string FirstName { get; set; }
+        //    public string LastName { get; set; }
+        //    public string Email { get; set; }
+        //    public string password { get; set; }
+
+        //}
+
+        //public class UserFavorite
+        //{
+        //    public string fips_code { get; set; }
+        //    public int userid { get; set; }
+
+        //}
+
+        public class MyClass
+        {
+            public List<CovidData> data { get; set; }
+        }
+
+        public class CovidData
+    {
+            public string data_as_of { get; set; }
+            public string startweek { get; set; }
+            public string endweek { get; set; }
+            public string state { get; set; }
+            public string county { get; set; }
+            public string fips_code { get; set; }
+            public int covid_19_deaths { get; set; }
+            public int total_deaths { get; set; }
+        }
+
+//    }
+}
