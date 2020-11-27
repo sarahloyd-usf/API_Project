1c823cf629548c62c7f9311b0586efde0fa9e90e
sarahloyd-usf <sarahloyd@usf.edu>
2020-11-27 12:22:10 -0500

    Added CovidDataID, added State data, fixed connection string

--- API_Project/Models/App_Models.cs
+++ API_Project/Models/App_Models.cs
@@ -7,29 +7,40 @@ namespace API_Project.Models
 {
         public class User
         {
-            public int userid { get; set; }
+            public int UserID { get; set; }
             public string FirstName { get; set; }
             public string LastName { get; set; }
             public string Email { get; set; }
-            public string password { get; set; }
-            public List<UserFavorite> Favorites { get; set; }
-
-        }
+            public string Password { get; set; }
+        //            public List<UserFavorite> Favorites { get; set; }
+        public virtual ICollection<UserFavorite> Favorites { get; set; }
+    }
 
         public class UserFavorite
         {
-            public string fips_code { get; set; }
-            public int userid { get; set; }
+            public int CovidDataID { get; set; }
+            public int UserID { get; set; }
+
+            public virtual ICollection<User> Users { get; set; }
+            public virtual ICollection<CovidData> CovidDatas { get; set; }
 
         }
 
-        public class MyClass
-        {
-            public List<CovidData> data { get; set; }
-        }
+    public class State
+    {
+        public string state { get; set; }
+
+        public virtual ICollection<CovidData> CovidDatas { get; set; }
+
+    }
+    public class MyClass
+            {
+                public List<CovidData> data { get; set; }
+            }
 
         public class CovidData
-    {
+        {
+            public int CovidDataID { get; set; }
             public string data_as_of { get; set; }
             public string startweek { get; set; }
             public string endweek { get; set; }
@@ -38,6 +49,8 @@ namespace API_Project.Models
             public string fips_code { get; set; }
             public int covid_19_deaths { get; set; }
             public int total_deaths { get; set; }
-        }
+
+        public virtual ICollection<UserFavorite> Favorites { get; set; }
+       }
 
 }
